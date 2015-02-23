4764f3d6fac71d05432cc59aa89ec5682272d3e5
gLENTNER <geoffrey.lentner@louisville.edu>
2015-02-22 21:12:48 -0500

    initial commit

--- Ehrenfest.py
+++ Ehrenfest.py
@@ -1,115 +1,330 @@
 #!/usr/bin/env python
+# Copyright (c) Geoffrey Lentner 2015. All Rights Reserved.
+# See LICENSE (GPL v2)
 """
+usage: Ehrenfest.py <*.dat files> [**kwargs]
+
 Visualize data output of EhrenfestModel code.
 """
 
 from __future__ import print_function
-from sys import version_info
-
 import matplotlib, numpy as np, os, sys, fnmatch
-from matplotlib import pyplot as plot 
+from matplotlib import pyplot as plot
 from scipy.interpolate import interp1d as interp
-
 matplotlib.rcParams['figure.facecolor'] = 'w'
-plot.ion()
 
 class ArgumentError(Exception):
+	"""
+	Derived from Exception
+	"""
 	pass
 
-def GetData( input_file, **kwargs ):
+def GetData( infile, **kwargs ):
 	"""
-	Extract the data from the file, input_file.
-
-	Arguments: input_file: <class 'str'>
-	              Full path to input file 
-
-	Returns:   equilibrium: <class 'numpy.ndarray'>
-	              Array containing number of steps to equilibrium for each 
-					  iteration.
-
-	           poincare: <class 'numpy.ndarray'>
-				     Array containing number of steps of poincare cycle for each
-					  iteration.
-
-	           history: <class 'numpy.ndarray'>
-	              2D array with rows for each iteration. Columns number one more
-					  than size of N particles. Represents system state history.
-
-	Options: None
+	Extract the data from the file, infile.
 	"""
-
 	try:
-		data = np.loadtxt( input_file )
+		return np.loadtxt( infile )
 
-	except IOError as err:
-		print(err.message)
-		print('\n Error loading %' % input_file)
+	except IOError as error:
+		print( error )
+		print('From file  %' % input_file)
 
-	equilibrium = data[:,0]
-	poincare    = data[:,1]
-	history     = data[:,2:]
-	iterations  = np.shape(data)[0]
-
-	return equilibrium, poincare, history, iterations
-
-def GetFiles( top_level, **kwargs ):
+def CyclePlot(data, N=None, **kwargs):
 	"""
-	Fetch the full path names of data (.dat) files below the 'top_level'
-	directory.
+	CyclePlot( data, N=None, **kwargs):
 
-	Arguments: top_level: <class 'str'>
-	              Name or relative path of top level directory to search
-					  under for data files.
+	If N=None, *data* is assumed to represent a single data set representing 
+	N particles. The elements are trials of the Ehrenfest model simulation. 
+	The function outputs a plot to *outfile* of frequency as a function of 
+	steps. If *N* is a list, representing the N particles for the data sets 
+	in the list *data*, it outputs a plot of mean steps to the cycle time as 
+	a function of N. 
 
-	Returns:   file_paths: <class 'list'>
-	              List of full paths to data files below top level
+	Arguments: data: 
+	               A 1D array or a list of such arrays.
+
+				  N: (default=None)
+	               None or list of integer sizes.
 	
-	Options:   pattern: <class 'str'>
-					Default is '*.dat'
-	"""
+	Options: resolution ( default = 1000 )
+	             Number of points for resampled fixed grid.
+				
+	         marker:
 
-	# dictionary of available parameters and default arguments.
-	params = {
-		'pattern':'*.dat'		
+	         xlabel:
+
+	         ylabel:
+
+	         title:
+
+	         fontsize:
+
+	         outfile:
+
+	         usetex:
+
+	         xscale:
+
+	         yscale:
+
+	"""
+	# check argument types
+	if type(data) is list:
+		for subset in data:
+			if type(subset) is not np.float_:
+				raise TypeError('Expected numpy.ndarray for list.')
+		if not N or len(N) != len(data):
+			raise ValueError('N must have length equal to *data*.')
+	elif type(data) is not np.ndarray:
+		raise TypeError('Expected numpy.ndarray if not list.')
+
+	# set default xlabel and ylabel
+	if N:
+		xlabel = 'N'
+		ylabel = 'steps'
+	else:
+		xlabel = 'steps'
+		ylabel = 'frequency'
+
+	# define allowed kwargs 
+	options = {
+		'resolution': '1000',
+		'marker':      'b-',
+		'xlabel':     xlabel,
+		'ylabel':     ylabel,
+		'title':      '',
+		'fontsize':   '14',
+		'outfile':    '',
+		'usetex':     'True',
+		'xscale':     'linear',
+		'yscale':     'linear'
 	}
 
-	# check key word arguments
-	for argument in kwargs:
-		if argument not in params:
-			raise ArgumentError('% is not a recognized parameter!' % argument)
-		if type(kwargs[argument]) is not type(params[argument]):
-			raise ArgumentError('% expects %' % (argument, type(params[argument])))
-		params[argument] = kwargs[argument]
+	# accept reassignments  
+	for keyword in kwargs:
+		if keyword not in options:
+			raise ArgumentError('{} is not a valid argument!'.format(keyword))
+		options[keyword] = kwargs[keyword]
+	
+	# assign with type correctness
+	marker     = options['marker']
+	xlabel     = options['xlabel']
+	ylabel     = options['ylabel']
+	title      = options['title']
+	fontsize   = int( options['fontsize'])
+	outfile    = options['outfile']
+	xscale     = options['xscale']
+	yscale     = options['yscale']
+	usetex     = True
+	if options['usetex'] == 'False':
+		usetex = False
 
-	# walk directory tree and find data files
-	file_paths = []
-	for dirname, dirnames, filenames in os.walk(top_level):
-		for filename in fnmatch.filter(filenames, params['pattern']):
-			file_paths.append( os.path.join(dirname, filename) )
-
-	return file_paths
-
-def PoincarePlot(data, **kwargs):
-	"""
-	Create smooth curve of Poincare cycle times.
-
-	Arguments: data: <class 'numpy.ndarray'> or <class 'list'>
-	              A one dimensional array containing the counts for the number 
-	              of steps to the poincare cycle. Can also be a list containing
-					  such arrays.
-
-	Options:
-	"""
-
-	if data is list:
-		for i, sample in enumerate(data):
-			mean   = sample.sum() / len(sample)
-			x      = range(  )
-			smooth = interp(x, sample, kind='cubic')
-			plot.plot(x, smooth(x))
+	if not N:
+		# plot frequency curve vs steps 
+		hist, edges = np.histogram(data)
+		hist = np.array(hist, dtype=np.float_)
+		centers = edges[:-1] + 0.5*( edges[1:] - edges[:-1] )
+		x, y = centers, hist / hist.sum( ) 
+	
 	else:
-		x = range( np.shape( data )[0])
-		smooth = interp(x, data, kind='cubic')
-		plot.plot(x, smooth(x))
+		# plot mean steps vs N, must sort by N
+		mixed = np.array([N, data])
+		x, y = list( mixed[:, mixed[0].argsort( ) ] )
 
-#if __name__ == '__main__':
+	# build plot
+	plot.plot( x, y, marker )
+	plot.title(title, fontsize=fontsize)
+	plot.xlabel(xlabel, fontsize=fontsize, labelpad=10)
+	plot.ylabel(ylabel, fontsize=fontsize, labelpad=10)
+	plot.xscale(xscale)
+	plot.yscale(yscale)
+
+	if usetex:
+		# latex type setting 
+		plot.rc('text', usetex=True)
+		plot.rc('font', family='serif')
+
+	if outfile:
+		# must be specified
+		plot.savefig(outfile, format=outfile.split('.')[-1] )
+
+def Poincare( data, N=None, **kwargs ):
+	"""
+	Handle for plotting Poincare cycle times. Derived from CyclePlot.
+	"""
+	CyclePlot( data, N, **kwargs )
+
+def Equilibrium( data, N=None, **kwargs ):
+	"""
+	Handle for plotting Equilibrium cycle times. Derived from CyclePlot.
+	"""
+	CyclePlot( data, N, **kwargs )
+
+def Distribution( data, **kwargs ):
+	"""
+	Construct plot of history for distribution of particles
+	"""
+	# available kwarg options 
+	options = {
+		'title'    : '',
+		'xlabel'   : 'distribution (box1:box2)',
+		'ylabel'   : 'frequency',
+		'marker'   : 'b-',
+		'fontsize' : '14',
+		'outfile'  : '',
+		'usetex'   : 'True',
+		'xscale'   : 'linear',
+		'yscale'   : 'linear'
+	}
+
+	# accept overides from user
+	for keyword in kwargs:
+		if keyword not in options:
+			raise ArgumentError(
+				'{} is not a valid argument for Distribution function.'
+				''.format(keyword) )
+		
+		options[keyword] = kwargs[keyword]
+	
+	# define parameters with correct type
+	title    = options['title']
+	xlabel   = options['xlabel']
+	ylabel   = options['ylabel']
+	marker   = options['marker']
+	fontsize = int( options['fontsize'])
+	outfile  = options['outfile']
+	xscale   = options['xscale']
+	yscale   = options['yscale']
+
+	if options['usetex'] == 'True':
+		usetex = True
+	elif options['usetex'] == 'False':
+		usetex = False
+	else:
+		raise TypeError(
+			'Distribution function expects type bool for '
+			'usetex option.'
+		)
+
+	# the user must provide outfile to save the figure
+	if not outfile:
+		raise ArgumentError(
+			'The user must provide the name of the output file '
+			'(e.g., "outfile=myplot.pdf").'
+		)
+
+	# check *data* type
+	if type(data) is not np.ndarray or len( np.shape(data) ) != 2:
+		raise TypeError(
+			'Distribution function expects 2D numpy.ndarray. See usage '
+			'for more details. Given numpy.shape(data)'
+			' = {}'.format( np.shape(data) )
+		)
+
+	# The sum of many trials avoids bias
+	plot.plot( data.sum(axis=0) / data.sum( ), marker )
+	plot.title( title, fontsize=fontsize )
+	plot.xlabel( xlabel, fontsize=fontsize, labelpad=10 )
+	plot.ylabel( ylabel, fontsize=fontsize, labelpad=10 )
+	plot.xscale( xscale )
+	plot.yscale( yscale )
+
+	if usetex:
+		plot.rc('text', usetex=True)
+		plot.rc('font', family='serif')
+
+	plot.savefig( outfile, format=outfile.split('.')[-1] )
+
+def Parse( *argv ):
+	"""
+	Parse sys.argv to seperate args and kwargs
+	"""
+	args = [ ]
+	kwargs = { }
+	for item in list( *argv ):
+		if type(item) is not str:
+			raise TypeError('Parse function only takes string arguments!')
+		part = item.split('=')
+		if len(part) == 1:
+			args.append(part[0])
+		elif len(part) == 2:
+			kwargs[part[0]] = part[1]
+		else:
+			raise ArgumentError('Too many "=" in <{}>.'.format(item))
+
+	return args, kwargs
+
+if __name__ == '__main__':
+
+	# display usage
+	if len( sys.argv ) == 1:
+		print( __doc__ )
+		sys.exit(0)
+
+	# available executables ( @functions )
+	executables = {
+		'Equilibrium':  Equilibrium.__doc__ + CyclePlot.__doc__,
+		'Poincare':     Poincare.__doc__   + CyclePlot.__doc__,
+		'Distribution': Distribution.__doc__
+		}
+
+	# get function name
+	function = sys.argv[1].split('@')
+	if len(function) != 2 or function[0] != '':
+		print('Error in function argument. Execute "Ehrenfest.py" without'
+				'arguments to see usage.'
+		)
+		sys.exit(1)
+	elif function[1] not in executables:
+		print('Error in function argument. That was not a recognized call'
+				' to an available function.'
+		)
+		sys.exit(1)
+	else: function = function[1]
+
+	# display function usage if no arguments
+	if len(sys.argv) < 3 or sys.argv[2] == "-h" or sys.argv[2] == "help":
+		print( executables[function] )
+		sys.exit(0)
+
+	# convert remaining arguments
+	args, kwargs = Parse( sys.argv[2:] )
+
+	if function == 'Poincare' or function == 'Equilibrium':
+		
+		# import data from files
+		data = [ GetData(fpath) for fpath in args ]
+		
+		# extract cycle time column from data sets
+		col = 0 if function == 'Equilibrium' else 1
+		cp_data = [ subset[:,col] for subset in data ]
+		
+		# if there are multiple data files (args) plot dependence
+		# on 'N', otherwise plot frequency vs steps.
+		if len( cp_data ) == 1:
+			cp_data = cp_data[0]
+			N = None
+		else:
+			cp_data = [ sample.mean( ) for sample in cp_data ]
+			N = [ np.shape(subset[:,2:])[1] - 1 for subset in data ]
+
+		# Choose function for plotting
+		Plot = Equilibrium if function == 'Equilibrium' else Poincare
+		Plot( cp_data, N, **kwargs )
+
+	elif function == 'Distribution':
+
+		# Check for single input file argument
+		if len(args) > 1:
+			raise ArgumentError(
+				'Only one data file can be used at a time with '
+				'Distribution function. See usage for me details.'
+			)
+		
+		# import data from file 
+		data = GetData( args[0] )
+
+		# extract simulation history from data, 
+		# summing sufficient number of trials avoids bias
+		Distribution( data[:,2:], **kwargs )
---
+++ __init__.py
@@ -0,0 +1,1 @@
+# Allows users to import from Ehrenfest.py
